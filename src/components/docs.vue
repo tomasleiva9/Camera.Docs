e42854d5d5a35f6c0241396287060d09fc2a1e24
Tomás Leiva <tomas.leivad@gmail.com>
2017-11-09 14:36:01 -0200

    Comprovante

--- src/components/docs.vue
+++ src/components/docs.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="">
     <div class="container">
-      <button type="button" name="button" @click="openCamera">abrir câmera</button>
+      <button type="button" name="button" @click="openCNH">Documento</button>
+      <button type="button" name="button" @click="openComprovante">Comprovante</button>
     </div>
 
     <q-modal ref="modal" minimized>
@@ -79,8 +80,11 @@ export default {
     Loading
   },
   methods: {
-    openCamera () {
-      navigator.camera.getPicture(this.cameraSuccess, this.cameraError)
+    openCNH () {
+      navigator.camera.getPicture(this.cnhSuccess, this.cameraError)
+    },
+    openComprovante () {
+      navigator.camera.getPicture(this.comprovanteSuccess, this.cameraError)
     },
     fecharModal () {
       this.doc = {};
@@ -94,7 +98,7 @@ export default {
                           this.doc.person.numRegistro ].join('\n\r')
       window.plugins.socialsharing.share(mensagem)
     },
-    cameraSuccess (img) {
+    cnhSuccess (img) {
       const _this = this
       Loading.show()
 
@@ -140,6 +144,54 @@ export default {
         }, function(e){console.error(e)});
       }, function(e){console.error(e)});
 
+    },
+    comprovanteSuccess (img) {
+      const _this = this
+      Loading.show()
+
+      const fd = new FormData();
+      window.resolveLocalFileSystemURL(img, function(fileEntry) {
+        fileEntry.file(function(file) {
+          var reader = new FileReader();
+          reader.onloadend = function(e) {
+            var imgBlob = new Blob([ this.result ], { type: "image/jpeg" } );
+            fd.append('file', imgBlob, file.name);
+            fd.append('cpfH', '36839854884');
+
+            $.ajax({
+              url: 'http://portal.nxcd.com.br/api/proofofresidence/evaluate/',
+              data: fd,
+              cache: false,
+              contentType: false,
+              processData: false,
+              method: 'POST',
+              mimeType: "multipart/form-data",
+              headers: {
+                "x-csrf-token": localStorage.getItem('token')
+              },
+              success: function(data){
+                debugger;
+                _this.doc = JSON.parse(data);
+                _this.$refs.modal.open();
+                Toast.create.positive({
+                  html: 'Leitura feita com sucesso.'
+                })
+                Loading.hide()
+              },
+              error: function(data){
+                Toast.create.negative({
+                  html: 'Não foi possível fazer a leitura. Tente novamente.'
+                })
+                Loading.hide()
+              }
+            });
+
+          };
+          reader.readAsArrayBuffer(file);
+
+        }, function(e){console.error(e)});
+      }, function(e){console.error(e)});
+
     }
   }
 }
@@ -148,10 +200,11 @@ export default {
 <style lang="stylus">
 .container
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 100vh;
-  background-color: tomato
+  background-color: tomato;
 
   button
     background-color: white;
@@ -160,6 +213,8 @@ export default {
     font-size: 20px;
     font-weight: bold;
     color: tomato;
+    min-width: 200px
+    margin: 20px
 
     &:focus,
     &:active
